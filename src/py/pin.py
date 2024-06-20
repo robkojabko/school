e31c09e782206fd195c3cfe2f2bdd00305d71303
robkojabko <75027870+robkojabko@users.noreply.github.com>
2024-06-20 08:37:50 +0200

    Upraven způsob ověření PIN

--- src/py/pin.py
+++ src/py/pin.py
@@ -1,15 +1,20 @@
 pin = 4851
 attempts = 3
-inp = int(None)
+inp = None
 corr = False
 
 print("***Vítejte v naší bance***")
 print("Pro přístup k vašemu účtu je nutné zadat čtyřmístný PIN")
 print("V případě zadání tří neplatných hodnot bude účet zablokován\n")
 if not corr:
-    for i in range(0,attempts-1):
+    for i in range(0,attempts):
+        if i < 0:
+            print("Špatný PIN")
         print("Zadejte PIN")
-        inp = input()
+        inp = int(input())
+        while len(str(inp)) != 4:
+            print("Špatná délka PIN! Vaše délka:", len(str(inp)), ". Očekávaná délka:", len(str(pin)))
+            inp = input()
         if inp == pin:
             corr = 1
             break
