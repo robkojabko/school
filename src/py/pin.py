9f309f4d0f726242c5e41e6065758d3854c26e5a
robkojabko <75027870+robkojabko@users.noreply.github.com>
2024-06-20 08:19:52 +0200

    přidány podmínky po zadání pin

--- src/py/pin.py
+++ src/py/pin.py
@@ -1,12 +1,20 @@
 pin = 4851
 attempts = 3
 inp = int(None)
+corr = bool(None)
 
 print("***Vítejte v naší bance***")
 print("Pro přístup k vašemu účtu je nutné zadat čtyřmístný PIN")
 print("V případě zadání tří neplatných hodnot bude účet zablokován\n")
-for i in range(0,attempts-1):
-    print("Zadejte PIN")
-    inp = input()
-    if inp == pin:
-        break
+if not corr:
+    for i in range(0,attempts-1):
+        print("Zadejte PIN")
+        inp = input()
+        if inp == pin:
+            corr = 1
+            break
+
+if not corr:
+    print("Třirát jste zadali nesprávný PIN, váš účet je zablokován")
+else:
+    print("Jupí jste ve svém účtě!!!")
